8eac49ba4df077cd7521aa6eb8a1f0b136a06088
Dynosol <sol.evergreen@gmail.com>
2024-02-28 04:13:01 -0500

    initial commit

---
+++ src/components/Modal.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { createPortal } from 'react-dom'
+
+const modalRoot = document.getElementById('modal')
+
+export default function Modal() {
+  // access modal root element with
+  // this is where the modal should be rendered and destroyed
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(prev => !prev);
+  };
+
+  return (
+    <>
+      {
+        // TODO: handle button click to change state
+      }
+      <button onClick={toggleModal}>Click Me!</button>
+
+      {
+        // TODO: conditionally show this based on open state
+      }
+      
+      {isOpen && createPortal(
+        <div
+          style={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: '#fff',
+            width: '20rem',
+            height: '20rem',
+            padding: '1rem',
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+          }}
+        >
+          Modal content
+          <button onClick={toggleModal}>Close</button>
+        </div>,
+        modalRoot as Element,
+      )}
+    </>
+  )
+}
